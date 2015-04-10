337b5c252accee7abfe9f432968a7e36bccffa35
bmcneely <brianjmcneely@gmail.com>
2015-04-10 13:33:19 -0400

    created and incorporated GameSpace.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+var gameSpace;
+
 $(document).ready(function() {
+	var viewPane = $("#view-pane");
+    gameSpace = new GameSpace(viewPane);
+	
     $("#vertical").kendoSplitter({
         orientation: "vertical",
         panes: [
@@ -19,63 +24,9 @@ $(document).ready(function() {
     // Add resize events to splitters
     var splitter;
     splitter = $("#vertical").data("kendoSplitter");
-    splitter.bind("resize", onViewResize);
+    splitter.bind("resize", gameSpace.onViewResize.bind(gameSpace));
     splitter = $("#horizontal").data("kendoSplitter");
-    splitter.bind("resize", onViewResize);
-
-    init();
-    animate();
+    splitter.bind("resize", gameSpace.onViewResize.bind(gameSpace));
+	
+    gameSpace.animate();
 });
-
-var camera, scene, renderer;
-var mesh;
-
-function init() {
-
-    var width = $("#view-pane").width();
-    var height = $("#view-pane").height();
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( width, height );
-    $("#view-pane").append(renderer.domElement);
-
-    camera = new THREE.PerspectiveCamera( 70, width / height, 1, 1000 );
-    camera.position.z = 400;
-
-    scene = new THREE.Scene();
-
-    var geometry = new THREE.BoxGeometry( 200, 200, 200 );
-
-    var texture = THREE.ImageUtils.loadTexture( 'data/textures/crate.gif' );
-    texture.anisotropy = renderer.getMaxAnisotropy();
-
-    var material = new THREE.MeshBasicMaterial( { map: texture } );
-
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
-
-}
-
-function onViewResize() {
-
-    var width = $("#view-pane").width();
-    var height = $("#view-pane").height();
-
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( width, height );
-
-}
-
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    mesh.rotation.x += 0.005;
-    mesh.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-
-}
